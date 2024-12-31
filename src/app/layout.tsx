5141b2eb281087d54c8497527d93089eb18753b3
Elias Nau <elias@codity.net>
2024-12-31 15:05:16 +0100

    feat: metadata changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans, Playfair_Display, Caveat } from "next/font/google";
 import "./globals.css";
 import { CSPostHogProvider } from '@/providers/posthog'
 import { Toaster } from "@/components/ui/toaster";
+import JsonLd from "@/components/global/JsonLd";
 
 const openSans = Open_Sans({ subsets: ["latin"], variable: "--font-opensans" });
 const playfair = Playfair_Display({ subsets: ["latin"], variable: "--font-playfair" });
@@ -11,6 +12,27 @@ const caveat = Caveat({ subsets: ["latin"], variable: "--font-caveat" });
 export const metadata: Metadata = {
   title: "Cobbers Garden",
   description: "Professionelle Gartenpflege",
+  metadataBase: new URL('https://cobbersgarden.de'),
+  openGraph: {
+    title: 'Cobbers Garden',
+    description: 'Professionelle Gartenpflege',
+    url: 'https://cobbersgarden.de',
+    siteName: 'Cobbers Garden',
+    images: [
+      {
+        url: 'https://cobbersgarden.de/assets/img/logo_full.png',
+        width: 1200,  
+        height: 630,
+      },
+      {
+        url: 'https://cobbersgarden.de/assets/img/hero_bg.jpg',
+        width: 1200,
+        height: 630,
+      }
+    ],
+    locale: 'de_DE',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +42,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
+      <head>
+        <JsonLd />
+      </head>
       <body className={`${openSans.variable} ${playfair.variable} ${caveat.variable} font-opensans`}>
         <CSPostHogProvider>
           <main>{children}</main>
